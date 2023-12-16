f884fb942994c7a22cca398af678bbcc76bd6b19
selcuk <slck.aydi@gmail.com>
2023-12-16 17:54:10 +0000

    Update

--- include/checked_shared_ptr.hpp
+++ include/checked_shared_ptr.hpp
@@ -4,6 +4,15 @@
 namespace sia
 {
     template <typename T>
+    struct MyPtr : public std::shared_ptr<T>
+    {
+    };
+
+    struct CheckedNullPtrException : std::exception
+    {
+    };
+
+    template <typename T>
     class checked_shared_ptr final
     {
         template <typename U>
@@ -14,12 +23,24 @@ namespace sia
         template <typename... Args>
         using Constructible = std::enable_if_t<std::is_constructible_v<std::shared_ptr<T>, Args...>>;
 
+        template <typename... Args>
+        using Assignable = std::enable_if_t<std::is_assignable_v<std::shared_ptr<T>, Args...>>;
+
     public:
         using element_type = typename std::shared_ptr<T>::element_type;
 
         // Conversion constructor.
         //
         checked_shared_ptr(const std::shared_ptr<T> &ptr) : m_ptr(ptr) {}
+
+        template <typename U, typename = std::enable_if_t<std::is_convertible_v<U *, T *>>>
+        checked_shared_ptr(const std::shared_ptr<U> &ptr) : m_ptr(ptr) {}
+
+        // template <typename... Args, typename = std::enable_if_t<std::is_constructible_v<std::shared_ptr<T>, Args...>>>
+        // checked_shared_ptr(Args &&...args)
+        // {
+        //     m_ptr = std::make_shared<T>(std::forward<Args>(args)...);
+        // }
 
         constexpr checked_shared_ptr() noexcept = default;
         constexpr checked_shared_ptr(std::nullptr_t) noexcept {}
@@ -42,5 +63,237 @@ namespace sia
 
         template <typename U, typename = Constructible<std::shared_ptr<U>>>
         checked_shared_ptr(checked_shared_ptr<U> &&r) noexcept : m_ptr(std::move(r)) {}
+
+        checked_shared_ptr &operator=(const checked_shared_ptr &) noexcept = default;
+
+        template <typename U, typename = Assignable<const std::shared_ptr<U> &>>
+        checked_shared_ptr &operator=(const checked_shared_ptr<U> &r)
+        {
+            m_ptr = r;
+        }
+
+        checked_shared_ptr &operator=(checked_shared_ptr &&r) noexcept
+        {
+            m_ptr = r;
+        }
+
+        template <typename U, typename = Assignable<std::shared_ptr<U>>>
+        checked_shared_ptr &operator=(checked_shared_ptr &&r) noexcept
+        {
+            m_ptr = r;
+        }
+
+        void reset() noexcept
+        {
+            m_ptr.reset();
+        }
+
+        template <typename U, typename = std::enable_if_t<std::is_convertible_v<U *, T *>>>
+        void reset(U *ptr) noexcept
+        {
+            m_ptr.reset(ptr);
+        }
+
+        void swap(checked_shared_ptr &r) noexcept
+        {
+            m_ptr.swap(r);
+        }
+
+        element_type *get() const noexcept
+        {
+            return m_ptr.get();
+        }
+
+        element_type &operator*() const noexcept(false)
+        {
+            throwIfNullPtr();
+            return *m_ptr;
+        }
+
+        element_type *operator->() const noexcept(false)
+        {
+            throwIfNullPtr();
+            return m_ptr.get();
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return m_ptr.operator bool();
+        }
+
+        auto managedSharedPointer() const noexcept
+        {
+            return m_ptr;
+        }
+
+    private:
+        inline void throwIfNullPtr() const noexcept(false)
+        {
+            if (get() == nullptr)
+                throw CheckedNullPtrException();
+        }
     };
+
+    template <typename Ch, typename Tr, typename Tp>
+    inline std::basic_ostream<Ch, Tr> &operator<<(std::basic_ostream<Ch, Tr> &os,
+                                                  const checked_shared_ptr<Tp> &p)
+    {
+        os << p.get();
+        return os;
+    }
+
+    template <typename T>
+    inline bool operator==(const ::sia::checked_shared_ptr<T> &lhs, std::nullptr_t) noexcept
+    {
+        return lhs.get() == nullptr;
+    }
+
+    template <typename T>
+    inline bool operator==(std::nullptr_t, const ::sia::checked_shared_ptr<T> &lhs) noexcept
+    {
+        return lhs.get() == nullptr;
+    }
+
+    template <typename T, typename U>
+    inline bool
+    operator==(const checked_shared_ptr<T> &lhs, const checked_shared_ptr<U> &rhs) noexcept(std::declval<checked_shared_ptr<T>>().get() == std::declval<checked_shared_ptr<U>>().get())
+    {
+        return lhs.get() == rhs.get();
+    }
+
+    template <typename T>
+    inline bool operator!=(const ::sia::checked_shared_ptr<T> &lhs, std::nullptr_t) noexcept
+    {
+        return lhs.get() != nullptr;
+    }
+
+    template <typename T>
+    inline bool operator!=(std::nullptr_t, const ::sia::checked_shared_ptr<T> &lhs) noexcept
+    {
+        return lhs.get() != nullptr;
+    }
+
+    template <typename T, typename U>
+    inline bool
+    operator!=(const checked_shared_ptr<T> &lhs, const checked_shared_ptr<U> &rhs) noexcept(std::declval<checked_shared_ptr<T>>().get() != std::declval<checked_shared_ptr<U>>().get())
+    {
+        return lhs.get() != rhs.get();
+    }
+
+    template <typename T, typename U>
+    inline bool operator<(const checked_shared_ptr<T> &lhs, const checked_shared_ptr<U> &rhs) noexcept
+    {
+        using LhsT = typename checked_shared_ptr<T>::element_type;
+        using RhsT = typename checked_shared_ptr<U>::element_type;
+        using RsT = std::common_type_t<LhsT *, RhsT *>;
+        return std::less<RsT>()(lhs.get(), rhs.get());
+    }
+
+    template <typename T>
+    inline bool operator<(const checked_shared_ptr<T> &r, std::nullptr_t) noexcept
+    {
+        using RsT = typename checked_shared_ptr<T>::element_type;
+        return std::less<RsT *>()(r.get(), nullptr);
+    }
+
+    template <typename T>
+    inline bool operator<(std::nullptr_t, const checked_shared_ptr<T> &r) noexcept
+    {
+        using RsT = typename checked_shared_ptr<T>::element_type;
+        return std::less<RsT *>()(r.get(), nullptr);
+    }
+
+    template <typename T, typename U>
+    inline bool operator<=(const checked_shared_ptr<T> &lhs, const checked_shared_ptr<U> &rhs) noexcept
+    {
+        return !(rhs < lhs);
+    }
+
+    template <typename T>
+    inline bool operator<=(const checked_shared_ptr<T> &r, std::nullptr_t) noexcept
+    {
+        return !(nullptr < r);
+    }
+
+    template <typename T>
+    inline bool operator<=(std::nullptr_t, const checked_shared_ptr<T> &r) noexcept
+    {
+        return !(r < nullptr);
+    }
+
+    template <typename T, typename U>
+    inline bool operator>(const checked_shared_ptr<T> &lhs, const checked_shared_ptr<U> &rhs) noexcept
+    {
+        return rhs < lhs;
+    }
+
+    template <typename T>
+    inline bool operator>(const checked_shared_ptr<T> &r, std::nullptr_t) noexcept
+    {
+        return nullptr < r;
+    }
+
+    template <typename T>
+    inline bool operator>(std::nullptr_t, const checked_shared_ptr<T> &r) noexcept
+    {
+        return r < nullptr;
+    }
+
+    template <typename T, typename U>
+    inline bool operator>=(const checked_shared_ptr<T> &lhs, const checked_shared_ptr<U> &rhs) noexcept
+    {
+        return !(lhs < rhs);
+    }
+
+    template <typename T>
+    inline bool operator>=(const checked_shared_ptr<T> &r, std::nullptr_t) noexcept
+    {
+        return !(r < nullptr);
+    }
+
+    template <typename T>
+    inline bool operator>=(std::nullptr_t, const checked_shared_ptr<T> &r) noexcept
+    {
+        return !(nullptr < r);
+    }
+
+    template <typename T>
+    inline void swap(checked_shared_ptr<T> &a, checked_shared_ptr<T> &b)
+    {
+        a.m_ptr.swap(b.m_ptr);
+    }
+}
+
+namespace std
+{
+    template <typename _Tp>
+    struct hash<sia::checked_shared_ptr<_Tp>>
+        : public __hash_base<size_t, shared_ptr<_Tp>>
+    {
+        size_t operator()(const sia::checked_shared_ptr<_Tp> &__s) const noexcept
+        {
+            return std::hash<typename sia::checked_shared_ptr<_Tp>::element_type *>()(__s.get());
+        }
+    };
+
+    template <typename T, typename U>
+    inline ::sia::checked_shared_ptr<T> static_pointer_cast(const ::sia::checked_shared_ptr<U> &__r) noexcept
+    {
+        using _Sp = ::sia::checked_shared_ptr<T>;
+        return _Sp(__r, static_cast<typename _Sp::element_type *>(const_cast<typename ::sia::checked_shared_ptr<U>::element_type *>(__r.get())));
+    }
+
+    template <typename T, typename U>
+    inline ::sia::checked_shared_ptr<T> dynamic_pointer_cast(const ::sia::checked_shared_ptr<U> &__r) noexcept
+    {
+        using _Sp = ::sia::checked_shared_ptr<T>;
+        return _Sp(__r, dynamic_cast<typename _Sp::element_type *>(const_cast<typename ::sia::checked_shared_ptr<U>::element_type *>(__r.get())));
+    }
+
+    template <typename T, typename U>
+    inline ::sia::checked_shared_ptr<T> const_pointer_cast(const ::sia::checked_shared_ptr<U> &__r) noexcept
+    {
+        using _Sp = ::sia::checked_shared_ptr<T>;
+        return _Sp(__r, static_cast<typename _Sp::element_type *>(const_cast<typename ::sia::checked_shared_ptr<U>::element_type *>(__r.get())));
+    }
 }
--- test/src/BasicTest.cpp
+++ test/src/BasicTest.cpp
@@ -1,12 +1,95 @@
 #include <gtest/gtest.h>
 #include "checked_shared_ptr.hpp"
 
-struct Object
+struct Person
 {
+    std::uint64_t m_id{};
+    std::string m_name{};
 
+    friend std::ostream& operator<<(std::ostream& os, Person& person);
+
+protected:
+    virtual ~Person() = default;
+};
+
+std::ostream& operator<<(std::ostream& os, Person& person)
+{
+    os << "Person=[(ID= " << person.m_id << ")(Name=" << person.m_name << ")]";
+    return os;
+}
+
+struct Developer final : Person
+{
+    std::uint8_t m_num_of_tasks{};
+
+    friend std::ostream& operator<<(std::ostream& os, Developer& developer);
+};
+
+std::ostream& operator<<(std::ostream& os, Developer& developer)
+{
+    os << "Developer=[" << static_cast<Person&>(developer) << "(Tasks=" << (std::uint32_t)developer.m_num_of_tasks << ")]";
+    return os;
+}
+
+struct Manager final : Person
+{
+    std::string m_current_task_name{};
+
+    friend std::ostream& operator<<(std::ostream& os, Manager& manager);
+};
+
+std::ostream& operator<<(std::ostream& os, Manager& manager)
+{
+    os << "Manager=[" << static_cast<Person&>(manager) << "(TaskName=" << manager.m_current_task_name << ")]";
+    return os;
+}
+
+struct Base
+{
+    ~Base()
+    {
+        std::cout << "~Base()\n";
+    }
+};
+
+struct Derived : Base
+{
+    ~Derived()
+    {
+        std::cout << "~Derived()\n";
+    }
 };
 
 TEST(Basic, test1)
 {
-    sia::checked_shared_ptr<Object> ptr = std::make_shared<Object>();    
+    sia::checked_shared_ptr<Person> developer = std::make_shared<Developer>();
+    sia::checked_shared_ptr<Person> manager = std::make_shared<Manager>();
+
+    developer->m_id = 20;
+    developer->m_name = "sia";
+
+    manager->m_id = 40;
+    manager->m_name = "john";
+
+    auto developer_ptr = std::static_pointer_cast<Developer>(developer);
+    auto manager_ptr = std::static_pointer_cast<Manager>(manager);
+
+    developer_ptr->m_num_of_tasks = 201;
+    manager_ptr->m_current_task_name = "digit handling";
+
+    std::cout << *developer_ptr << '\n';
+    std::cout << *manager_ptr << '\n';
+
+    auto valid_cast = std::dynamic_pointer_cast<Manager>(manager_ptr);
+    auto invalid_cast = std::dynamic_pointer_cast<Manager>(developer_ptr);
+
+    EXPECT_FALSE(valid_cast == nullptr);
+    EXPECT_TRUE(invalid_cast == nullptr);
+
+    Base base = std::move(Derived{});
+    base = Derived{};
+
+    static_assert(std::is_constructible_v<Base, Derived>, "");
+
+    EXPECT_THROW(invalid_cast->m_current_task_name, sia::CheckedNullPtrException);
 }
